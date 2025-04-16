c1c43b7333f836b4bd993fbfd6ad3910d061f73f
blinkEgor <barminov44@gmail.com>
2025-04-16 10:25:55 +0300

    Fix comments for virtual button class

--- src/UI/Button.h
+++ src/UI/Button.h
@@ -4,11 +4,12 @@
 
 class Button {
 protected:
-    SDL_Rect m_button_rect = { 0, 0, 10, 10 }; // Границы кнопки
-    SDL_Color m_background_color = { 0, 255, 0, 0 }; // Цвет кнопки
-    SDL_Color m_text_color = { 255, 255, 255, 0 }; // Цвет текста 
-    std::string m_text = "text"; // Текси кнопки
-    bool is_clicked = false; // Флаг нажатия
+    SDL_Rect m_button_rect = { 0, 0, 30, 10 }; // Границы кнопки: по умолчанию квадрад 30*10 в левом верхнем углу экрана
+    SDL_Color m_background_color = { 0, 255, 0, 0 }; // Цвет кнопки: по умолчанию зелёный
+    SDL_Color m_text_color = { 255, 255, 255, 0 }; // Цвет текста: по умолчанию белый
+    std::string m_text = "text"; // Текст кнопки: по умолчанию "text"
+    
+    bool is_clicked = false; // Флаг нажатия: по умолчанию "false"
 
 public:
     virtual void draw( SDL_Renderer* renderer ) = 0;
