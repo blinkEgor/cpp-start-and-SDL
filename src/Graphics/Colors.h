8dd5e45a30ccf9eb42d13b019db2c49df2b92444
blinkEgor <barminov44@gmail.com>
2025-04-22 09:55:04 +0300

    Create a color palette file with a namespace

---
+++ src/Graphics/Colors.h
@@ -0,0 +1,15 @@
+#include <SDL2/SDL.h>
+
+namespace Palette {
+    // Window
+    const SDL_Color CLEAR_WINDOW = { 0, 0, 0, 255 };
+
+    // UI
+    const SDL_Color TEXT_WHITE = { 220, 220, 220, 255 };
+    const SDL_Color BG_BTN_GREEN = { 50, 160, 50, 255 };
+
+    // Game
+    const SDL_Color GRID_LINE_GREY { 50, 50, 50, 255 };
+    const SDL_Color SNAKE_GREEN = { 100, 220, 50, 255 };
+    const SDL_Color FOOD_RED = { 255, 80, 60, 255 };
+}
